d2f8b45be5caa023b3bdf3405ec09e1ee42416e1
Helder Oliveira <heldrida@gmail.com>
2024-10-25 13:30:37 +0100

    test: 💍 add files to ipfs

--- src/clients/ipfs.test.ts
+++ src/clients/ipfs.test.ts
@@ -11,7 +11,7 @@ import { server } from '../mocks/graphql/node';
 import { mockGraphqlServiceApiUrl as graphqlServiceApiUrl } from '../mocks/graphql/handlers';
 import { FleekSdk } from '../FleekSdk';
 
-type MockFileNames = 'HelloWorld';
+type MockFileNames = 'HelloWorld' | 'Lyrics';
 type MockFiles = Record<MockFileNames, {
   path: string;
   content: Buffer;
@@ -20,20 +20,39 @@ type MockFiles = Record<MockFileNames, {
 
 const mockFiles: MockFiles = {
   HelloWorld: {
-    path: 'src/file.txt',
+    path: 'src/HelloWorld.txt',
     content: Buffer.from('Hello World!'),
     cid: 'QmWvQxTqbG2Z9HPJgG57jjwR154cKhbtJenbyYTWkjgF3e',
-  }
+  },
+  Lyrics: {
+    path: 'Documents/random/Lyrics.txt',
+    content: Buffer.from('My favourite song lyrics'),
+    cid: 'QmQtQq4iofjT8vgD75G3UPF3v4caatuFw3YnU2mhn17BUf',
+  },
 }
 
-const mockUploadContent = vi.fn().mockResolvedValue({
-  pin: {
-    cid: mockFiles.HelloWorld.cid,
-    size: 100
-  },
-  duplicate: false,
-});
+const mockUploadContent = vi.fn().mockImplementation(async ({ basename }) => {
+  if (basename === 'HelloWorld.txt') {
+    return {
+      pin: {
+        cid: mockFiles.HelloWorld.cid,
+        size: 100
+      },
+      duplicate: false,
+    };
+  }
 
+  if (basename === 'Lyrics.txt') {
+    return {
+      pin: {
+        cid: mockFiles.HelloWorld.cid,
+        size: 100
+      },
+      duplicate: false,
+    };
+  }
+});
+  
 vi.mock('./uploadProxy', () => {
   return {
     UploadProxyClient: vi.fn().mockImplementation(() => ({
@@ -102,12 +121,113 @@ describe('FleekSDK', () => {
           ],
           "version": 0,
         },
-        "path": "file.txt",
+        "path": "HelloWorld.txt",
         "size": 100,
       }
     `);
   });
 
-  it.todo('should add all files to ipfs', async () => {});
+  it('should add all files to ipfs', async () => {
+    const { HelloWorld, Lyrics } = mockFiles;
+
+    const response = await sdk.ipfs().addAll([
+      HelloWorld,
+      Lyrics,
+    ]);
+
+    expect(response).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "cid": Object {
+            "code": 112,
+            "hash": Uint8Array [
+              18,
+              32,
+              127,
+              131,
+              177,
+              101,
+              127,
+              241,
+              252,
+              83,
+              185,
+              45,
+              193,
+              129,
+              72,
+              161,
+              214,
+              93,
+              252,
+              45,
+              75,
+              31,
+              163,
+              214,
+              119,
+              40,
+              74,
+              221,
+              210,
+              0,
+              18,
+              109,
+              144,
+              105,
+            ],
+            "version": 0,
+          },
+          "path": "HelloWorld.txt",
+          "size": 100,
+        },
+        Object {
+          "cid": Object {
+            "code": 112,
+            "hash": Uint8Array [
+              18,
+              32,
+              127,
+              131,
+              177,
+              101,
+              127,
+              241,
+              252,
+              83,
+              185,
+              45,
+              193,
+              129,
+              72,
+              161,
+              214,
+              93,
+              252,
+              45,
+              75,
+              31,
+              163,
+              214,
+              119,
+              40,
+              74,
+              221,
+              210,
+              0,
+              18,
+              109,
+              144,
+              105,
+            ],
+            "version": 0,
+          },
+          "path": "Lyrics.txt",
+          "size": 100,
+        },
+      ]
+    `);
+  });
+
   it.todo('should add files by path', async () => {});
 });
