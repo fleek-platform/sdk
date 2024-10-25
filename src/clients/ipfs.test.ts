6077181bc74fe7f9bccd06372c4bbe47cc7d8794
Helder Oliveira <heldrida@gmail.com>
2024-10-25 13:18:09 +0100

    test: 💍 add file to ipfs

---
+++ src/clients/ipfs.test.ts
@@ -0,0 +1,113 @@
+import {
+  describe,
+  expect,
+  it,
+  afterAll,
+  afterEach,
+  beforeAll,
+  vi,
+} from 'vitest';
+import { server } from '../mocks/graphql/node';
+import { mockGraphqlServiceApiUrl as graphqlServiceApiUrl } from '../mocks/graphql/handlers';
+import { FleekSdk } from '../FleekSdk';
+
+type MockFileNames = 'HelloWorld';
+type MockFiles = Record<MockFileNames, {
+  path: string;
+  content: Buffer;
+  cid: string;
+}>;
+
+const mockFiles: MockFiles = {
+  HelloWorld: {
+    path: 'src/file.txt',
+    content: Buffer.from('Hello World!'),
+    cid: 'QmWvQxTqbG2Z9HPJgG57jjwR154cKhbtJenbyYTWkjgF3e',
+  }
+}
+
+const mockUploadContent = vi.fn().mockResolvedValue({
+  pin: {
+    cid: mockFiles.HelloWorld.cid,
+    size: 100
+  },
+  duplicate: false,
+});
+
+vi.mock('./uploadProxy', () => {
+  return {
+    UploadProxyClient: vi.fn().mockImplementation(() => ({
+      uploadContent: mockUploadContent
+    }))
+  };
+});
+
+describe('FleekSDK', () => {
+  const sdk = new FleekSdk({
+    graphqlServiceApiUrl,
+    accessTokenService: {} as any,
+  });
+
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  it('should add file to ipfs', async () => {
+    const { path, content } = mockFiles.HelloWorld;
+    
+    const response = await sdk.ipfs().add({
+      path,
+      content,
+    });
+
+    expect(response).toMatchInlineSnapshot(`
+      Object {
+        "cid": Object {
+          "code": 112,
+          "hash": Uint8Array [
+            18,
+            32,
+            127,
+            131,
+            177,
+            101,
+            127,
+            241,
+            252,
+            83,
+            185,
+            45,
+            193,
+            129,
+            72,
+            161,
+            214,
+            93,
+            252,
+            45,
+            75,
+            31,
+            163,
+            214,
+            119,
+            40,
+            74,
+            221,
+            210,
+            0,
+            18,
+            109,
+            144,
+            105,
+          ],
+          "version": 0,
+        },
+        "path": "file.txt",
+        "size": 100,
+      }
+    `);
+  });
+
+  it.todo('should add all files to ipfs', async () => {});
+  it.todo('should add files by path', async () => {});
+});
